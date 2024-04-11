d423bfdde6e46a23fe4cc6f370334ee470ad92b4
waifuZeroTwo <hoes.romeo@gmail.com>
2024-04-11 17:19:33 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet"/>
 
 function App() {
+  const [playerName, setPlayerName] = useState('');
+  const [playerStats, setPlayerStats] = useState(null);
+
+  // Function to handle search submissions
+  const handleSearch = async (name) => {
+    setPlayerName(name);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Valorant Stat Tracker</h1>
+        </header>
+        <main>
+          {}
+          <div>
+            {}
+            <input
+                type="text"
+                placeholder="Enter Player Name"
+                value={playerName}
+                onChange={(e) => setPlayerName(e.target.value)}
+            />
+            <button onClick={() => handleSearch(playerName)}>Search</button>
+          </div>
+          <div>
+            {}
+            {playerStats ? (
+                <div>
+                  {}
+                  <p>Player Stats for: {playerName}</p>
+                  {}
+                </div>
+            ) : (
+                <p>Enter a player name to see stats.</p>
+            )}
+          </div>
+        </main>
+      </div>
   );
 }
 
